e3d4a05e300d087d67a761a61fc49f53c4341217
Miles Johnson <mileswjohnson@gmail.com>
2025-10-21 21:13:22 -0700

    Add tabs.

--- web/src/mdxComponents.tsx
+++ web/src/mdxComponents.tsx
@@ -1,10 +1,12 @@
 import { TypeTable } from 'fumadocs-ui/components/type-table';
 import defaultComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { ConfigTabs } from '@/components/ConfigTabs';
 
 export function getMdxComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultComponents,
+		ConfigTabs,
 		TypeTable,
 		...components,
 	};
