37d6ee846d10b60da4c42cf7335086d1a1387d86
Miles Johnson <mileswjohnson@gmail.com>
2026-03-10 09:17:16 -0700

    Add PKL.

--- web/src/components/ConfigTabs.tsx
+++ web/src/components/ConfigTabs.tsx
@@ -3,6 +3,7 @@ import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import TOML from 'smol-toml';
 import YAML from 'yaml';
 import HCL, { type HclOptions } from '@/utils/hcl';
+import PKL, { type PklOptions } from '@/utils/pkl';
 
 export interface ConfigTabsProps {
 	config: object;
@@ -11,7 +12,10 @@ export interface ConfigTabsProps {
 
 export function ConfigTabs({ config, file }: ConfigTabsProps) {
 	return (
-		<Tabs groupId="config-format" items={['YAML', 'JSON', 'TOML', 'HCL']}>
+		<Tabs
+			groupId="config-format"
+			items={['YAML', 'JSON', 'TOML', 'HCL', 'PKL']}
+		>
 			<Tab value="JSON">
 				<Code
 					code={JSON.stringify(config, null, 2).trim()}
@@ -41,13 +45,35 @@ export function ConfigTabs({ config, file }: ConfigTabsProps) {
 
 			<Tab value="HCL">
 				<Code
-					code={HCL.stringify(config, getHclOptions(file)).trim()}
+					code={
+						HCL.stringify(config, getHclOptions(file)).trim() +
+						appendComment(
+							'# Note: We built a custom HCL formatter and this syntax may not be 100% accurate!',
+						)
+					}
 					codeblock={{ title: `${file}.hcl` }}
 					lang="hcl"
 				/>
 			</Tab>
+
+			<Tab value="PKL">
+				<Code
+					code={
+						PKL.stringify(config, getPklOptions(file)).trim() +
+						appendComment(
+							'// Note: We built a custom PKL formatter and this syntax may not be 100% accurate!',
+						)
+					}
+					codeblock={{ title: `${file}.pkl` }}
+					lang="pkl"
+				/>
+			</Tab>
 		</Tabs>
 	);
+}
+
+function appendComment(comment: string): string {
+	return `\n\n${comment}`;
 }
 
 function getHclOptions(fileName: string): HclOptions {
@@ -60,4 +86,32 @@ function getHclOptions(fileName: string): HclOptions {
 	}
 
 	return { labeledBlocks };
+}
+
+function getPklOptions(fileName: string): PklOptions {
+	const mappings: string[] = [];
+
+	if (fileName.startsWith('.moon/tasks') || fileName === 'moon') {
+		mappings.push(
+			'tasks',
+			'tasks.deps.env',
+			'tasks.env',
+			'env',
+			'fileGroups',
+			'owners.customGroups',
+			'workspace.inheritedTasks.rename',
+		);
+	} else if (fileName === 'template') {
+		mappings.push('variables');
+	} else if (fileName === '.moon/workspace') {
+		mappings.push(
+			'codeowners.globalPaths',
+			'constraints.tagRelationships',
+			'projects.sources',
+			'remote.auth.headers',
+			'vcs.hooks',
+		);
+	}
+
+	return { mappings };
 }
